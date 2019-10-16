60a0994f27448ce10a31d752ab949e336c5e275e
Eric Fleming <eric-fleming18@hotmail.com>
2019-10-16 17:19:12 -0400

    Adding ability to modify users roles

---
+++ DemoApp/DemoApp.UI/Pages/Admin/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using DemoApp.Core;
+using DemoApp.Infrastructure.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoApp.UI.Pages.Admin
+{
+    [Authorize(Roles = AuthorizationConstants.Roles.ADMINISTRATORS)]
+    public class IndexModel : PageModel
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public IndexModel(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            this._userManager = userManager;
+            this._roleManager = roleManager;
+        }
+
+        public List<ApplicationUser> Users { get; set; }
+        public List<IdentityRole> Roles { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            Users = _userManager.Users.ToList();
+            Roles = _roleManager.Roles.ToList();
+
+            return Page();
+        }
+    }
+}
---
+++ DemoApp/DemoApp.UI/wwwroot/js/admin.js
@@ -0,0 +1,22 @@
+﻿$(function () {
+
+    $('#removeUserModal').on('show.bs.modal', function (e) {
+        var button = $(e.relatedTarget);
+
+        var userEmail = button.attr('data-userEmail');
+        var userId = button.attr('data-userId');
+
+        $('#spnRemoveUserEmail').html(userEmail);
+        $('#hidRemoveUserId').val(userId);
+    });
+
+    $('#removeRoleModal').on('show.bs.modal', function (e) {
+        var button = $(e.relatedTarget);
+
+        var roleName = button.attr('data-roleName');
+        var roleId = button.attr('data-roleId');
+
+        $('#spnRemoveRoleName').html(roleName);
+        $('#hidRemoveRoleId').val(roleId);
+    });
+});
